d307a0972f0718048f5d75696a8bca3a841770bd
Suhani Vaidya <suhanivaidya3@gmail.com>
2026-01-22 01:07:06 +0530

    new

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -3,6 +3,5 @@
  * Centralized API URL configuration for all environment types
  */
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-
+const API_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
 export default API_URL;
